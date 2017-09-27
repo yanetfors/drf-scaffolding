7c75cfcb4cc1330608de15789f65b13c2f52108d
Angel Lagunas <angel.lagunas@ironbit.com.mx>
2017-09-26 19:21:25 -0500

    fix: init drf_scaffolding file

--- drf_scaffolding/__init__.py
+++ drf_scaffolding/__init__.py
@@ -1,7 +1,2 @@
-# -*- encoding:utf-8 -*-
-from django.db import models
-
-models.options.DEFAULT_NAMES += ('drf_config',)
-
 __author__ = u'Ángel Lagunas'
-__version__ = '0.0.13'
+__version__ = '0.0.14'
---
+++ drf_scaffolding/management/__init__.py
@@ -0,0 +1,4 @@
+# -*- encoding:utf-8 -*-
+from django.db import models
+
+models.options.DEFAULT_NAMES += ('drf_config',)
