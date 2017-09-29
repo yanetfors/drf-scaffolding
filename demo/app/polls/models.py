cf981c9c07b3f34642f754e1468154843a0de16f
Angel Lagunas <angel.lagunas@ironbit.com.mx>
2017-09-29 12:49:26 -0500

    refactor: update demo

---
+++ demo/app/polls/models.py
@@ -0,0 +1,51 @@
+# -*- coding_ utf-8 -*-
+
+from django.db import models
+
+
+class Poll(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=100)
+
+    class Meta:
+        drf_config = {
+            'api': {
+                'scaffolding': True,
+                'methods': ['CREATE', 'UPDATE']
+            },
+            'serializer': {
+                'scaffolding': True,
+                'fields': [
+                    'title'
+                ]
+            },
+            'form': {
+                'scaffolding': True
+            },
+            'admin': {
+                'scaffolding': True
+            }
+        }
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=100)
+    poll = models.ForeignKey(Poll, related_name='questions')
+
+    class Meta:
+        drf_config = {
+            'api': {
+                'scaffolding': True,
+                'methods': ['CREATE', 'UPDATE']
+            },
+            'serializer': {
+                'scaffolding': True
+            },
+            'form': {
+                'scaffolding': True
+            },
+            'admin': {
+                'scaffolding': True
+            }
+        }
